a30d439ccd1848ea0ad8b17cd20a7ccc1820cf3b
Mathias Lefebre <54477532+RadioArtz@users.noreply.github.com>
2024-06-20 04:49:14 +0200

    Begin Code Cleanup
    seperated out some repetetive calls into their own functions and generally seperated out some things to improve maintainability.
    also fixed an "issue" with transparency assignment using a hardcoded renderqueue value

--- Source2Unity Material Fixer/Source2UnityMatFixer.cs
+++ Source2Unity Material Fixer/Source2UnityMatFixer.cs
@@ -9,9 +9,9 @@ public class Source2UnityMatFixer : EditorWindow
 	public bool assignNormalMaps;
 	public bool useStandardSpecular;
 	public bool zeroSpecularValues;
-	public Shader shaderStandard;
-	public Shader shaderStandardSpecular;
 	public bool trySetTransparency;
+	private Shader shaderStandard;
+	private Shader shaderStandardSpecular;
 	private List<Material> failedMaterials = new List<Material>();
 
 	[MenuItem("RadioArtz/Source2Unity Utils/Material Fixer")]
@@ -41,36 +41,47 @@ public class Source2UnityMatFixer : EditorWindow
 		EditorGUILayout.PropertyField(materialsProperty, true);
 		EditorGUILayout.PropertyField(texturesProperty, true);
 		EditorGUILayout.PropertyField(useStandardSpecularProperty);
+
 		if (useStandardSpecular)
 		{
 			EditorGUILayout.PropertyField(zeroSpecularValuesProperty);
 		}
+
 		EditorGUILayout.PropertyField(assignNormalMapsProperty);
 		EditorGUILayout.PropertyField(trySetTransparencyProperty);
 
 		serializedObject.ApplyModifiedProperties();
 
+		DrawButtons();
+		DisplayFailedMaterials();
+	}
+
+	private void DrawButtons()
+	{
 		if (GUILayout.Button("Assign Textures"))
 		{
 			if (EditorUtility.DisplayDialog("Confirm Action", "Do you really want to assign textures?", "Yes", "No"))
-			{
 				AssignTexturesToMaterials();
-			}
 		}
 		if (GUILayout.Button("Fix All Normal Maps"))
 		{
 			if (EditorUtility.DisplayDialog("Confirm Action", "Do you really want to fix all Normalmaps? This will cause a reimport and unity will be unresponsive for a while.", "Yes", "No"))
-			{
 				FixAllNormalMaps();
-			}
 		}
 		if (GUILayout.Button("Unassign All Textures"))
 		{
 			if (EditorUtility.DisplayDialog("Confirm Action", "Do you really want to unassign all textures?", "Yes", "No"))
-			{
 				RemoveAllTexturesFromMaterials();
-			}
 		}
+		if (failedMaterials.Count > 0 && GUILayout.Button("Clear Failed Materials List"))
+		{
+			if (EditorUtility.DisplayDialog("Confirm Action", "Really Clear the Failed Materials List?", "Yes", "No"))
+				failedMaterials.Clear();
+		}
+	}
+
+	private void DisplayFailedMaterials()
+	{
 		if (failedMaterials.Count > 0)
 		{
 			GUILayout.Label("Materials that failed texture assignment:", EditorStyles.boldLabel);
@@ -79,150 +90,162 @@ public class Source2UnityMatFixer : EditorWindow
 				EditorGUILayout.ObjectField(material, typeof(Material), true);
 			}
 		}
-		if(failedMaterials.Count > 0)
-		{
-			if (GUILayout.Button("Clear Failed Materials List"))
-			{
-				if (EditorUtility.DisplayDialog("Confirm Action", "Really Clear the Failed Materials List?", "Yes", "No"))
-				{
-					failedMaterials.Clear();
-				}
-			}
-		}
 	}
 
 	private void AssignTexturesToMaterials()
 	{
 		failedMaterials.Clear();
 		Shader selectedShader = useStandardSpecular ? shaderStandardSpecular : shaderStandard;
+
 		foreach (var material in materials)
 		{
 			if (material == null) continue;
 
 			material.shader = selectedShader;
 			string materialName = material.name.ToLower();
-			bool textureAssigned = false;
-			foreach (var texture in textures)
-			{
-				if (texture == null) continue;
-				string textureName = texture.name.ToLower();
-				if (materialName.Equals(textureName) &&
-					!textureName.Contains("_normal") &&
-					!textureName.Contains("_spec") &&
-					!textureName.Contains("_specular"))
-				{
-					material.mainTexture = texture;
-					material.SetColor("_Color", Color.white);
-					Debug.Log($"Assigned {texture.name} to {material.name}");
-					textureAssigned = true;
-					break;
-				}
-			}
-			if (!textureAssigned)
-			{
-				foreach (var texture in textures)
-				{
-					if (texture == null) continue;
-					string textureName = texture.name.ToLower();
-					if (materialName.Contains(textureName) &&
-						!textureName.Contains("_normal") &&
-						!textureName.Contains("_spec") &&
-						!textureName.Contains("_specular"))
-					{
-						material.mainTexture = texture;
-						material.SetColor("_Color", Color.white);
-						Debug.Log($"Assigned {texture.name} to {material.name} (partial match)");
-						textureAssigned = true;
-						break;
-					}
-				}
-			}
+			bool textureAssigned = TryAssignMainTexture(material, materialName);
 			if (!textureAssigned)
 			{
 				Debug.LogWarning($"No suitable texture found for {material.name}. Check naming conventions or assign manually.");
 				failedMaterials.Add(material);
 			}
-			if (assignNormalMaps)
-			{
-				foreach (var texture in textures)
-				{
-					if (texture == null) continue;
 
-					string textureName = texture.name.ToLower();
-
-					if (textureName.Contains("_normal") && materialName.Contains(textureName.Replace("_normal", "")))
-					{
-						material.SetTexture("_BumpMap", texture);
-						material.EnableKeyword("_NORMALMAP");
-						Debug.Log($"Assigned normal map {texture.name} to {material.name}");
-						break;
-					}
-				}
-			}
-			if (useStandardSpecular && zeroSpecularValues)
-			{
-				material.SetColor("_SpecColor", Color.black);
-				material.SetFloat("_Glossiness", 0f);
-				Debug.Log($"Set specular color and glossiness to black and zero for {material.name}");
-			}
-			if (trySetTransparency)
-			{
-				if (materialName.Contains("stain") || materialName.Contains("glass"))
-				{
-					material.SetFloat("_Mode", 2);
-					material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-					material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-					material.SetInt("_ZWrite", 0);
-					material.DisableKeyword("_ALPHATEST_ON");
-					material.EnableKeyword("_ALPHABLEND_ON");
-					material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-					material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
-				}
-				else if (materialName.Contains("ivy") || materialName.Contains("rail") || materialName.Contains("truss") || materialName.Contains("alpha"))
-				{
-					material.SetFloat("_Mode", 1);
-					material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-					material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-					material.SetInt("_ZWrite", 1);
-					material.EnableKeyword("_ALPHATEST_ON");
-					material.DisableKeyword("_ALPHABLEND_ON");
-					material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-					material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.AlphaTest;
-				}
-				else
-				{
-					material.SetFloat("_Mode", 0);
-					material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-					material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-					material.SetInt("_ZWrite", 1);
-					material.DisableKeyword("_ALPHATEST_ON");
-					material.DisableKeyword("_ALPHABLEND_ON");
-					material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-					material.renderQueue = 2000;
-				}
-			}
-			else
-			{
-				material.SetFloat("_Mode", 0);
-				material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-				material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-				material.SetInt("_ZWrite", 1);
-				material.DisableKeyword("_ALPHATEST_ON");
-				material.DisableKeyword("_ALPHABLEND_ON");
-				material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-				material.renderQueue = 2000;
-			}
+			AssignNormalMap(material, materialName);
+			ApplySpecularSettings(material);
+			ApplyTransparencySettings(material, materialName);
 		}
 		Debug.Log("Texture assignment complete.");
 	}
+
+	private bool TryAssignMainTexture(Material material, string materialName)
+	{
+		foreach (var texture in textures)
+		{
+			if (texture == null) continue;
+
+			string textureName = texture.name.ToLower();
+			if (IsMainTextureMatch(materialName, textureName))
+			{
+				material.mainTexture = texture;
+				material.SetColor("_Color", Color.white);
+				Debug.Log($"Assigned {texture.name} to {material.name}");
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private bool IsMainTextureMatch(string materialName, string textureName)
+	{
+        //Try exact match first, then fallback to partial matching
+        if(materialName.Equals(textureName) &&
+			   !textureName.Contains("_normal") &&
+			   !textureName.Contains("_spec") &&
+			   !textureName.Contains("_specular"))
+               return true;
+        else if(materialName.Contains(textureName) &&
+			   !textureName.Contains("_normal") &&
+			   !textureName.Contains("_spec") &&
+			   !textureName.Contains("_specular"))
+               return true;
+        else
+            return false;
+	}
+
+	private void AssignNormalMap(Material material, string materialName)
+	{
+		if (!assignNormalMaps) return;
+
+		foreach (var texture in textures)
+		{
+			if (texture == null) continue;
+
+			string textureName = texture.name.ToLower();
+			if (textureName.Contains("_normal") && materialName.Contains(textureName.Replace("_normal", "")))
+			{
+				material.SetTexture("_BumpMap", texture);
+				material.EnableKeyword("_NORMALMAP");
+				Debug.Log($"Assigned normal map {texture.name} to {material.name}");
+				break;
+			}
+		}
+	}
+
+	private void ApplySpecularSettings(Material material)
+	{
+		if (useStandardSpecular && zeroSpecularValues)
+		{
+			material.SetColor("_SpecColor", Color.black);
+			material.SetFloat("_Glossiness", 0f);
+			Debug.Log($"Set specular color and glossiness to black and zero for {material.name}");
+		}
+		else if(useStandardSpecular)
+		{
+			material.SetColor("_SpecColor", Color.gray);
+			material.SetFloat("_Glossiness", 0.5f);
+			Debug.Log($"Set specular color and glossiness to gray and 0.5 for {material.name}");
+		}
+	}
+
+	private void ApplyTransparencySettings(Material material, string materialName)
+	{
+		if (trySetTransparency)
+		{
+			if (materialName.Contains("stain") || materialName.Contains("glass"))
+				SetMaterialToFadeTransparency(material);
+			else if (materialName.Contains("ivy") || materialName.Contains("rail") || materialName.Contains("truss") || materialName.Contains("alpha"))
+				SetMaterialToCutout(material);
+			else 
+				SetMaterialToOpaque(material);
+		}
+		else
+			SetMaterialToOpaque(material);
+	}
+
+	private void SetMaterialToFadeTransparency(Material material)
+	{
+		material.SetFloat("_Mode", 2);
+		material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+		material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+		material.SetInt("_ZWrite", 0);
+		material.DisableKeyword("_ALPHATEST_ON");
+		material.EnableKeyword("_ALPHABLEND_ON");
+		material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+		material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+	}
+
+	private void SetMaterialToCutout(Material material)
+	{
+		material.SetFloat("_Mode", 1);
+		material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+		material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+		material.SetInt("_ZWrite", 1);
+		material.EnableKeyword("_ALPHATEST_ON");
+		material.DisableKeyword("_ALPHABLEND_ON");
+		material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+		material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.AlphaTest;
+	}
+
+	private void SetMaterialToOpaque(Material material)
+	{
+		material.SetFloat("_Mode", 0);
+		material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+		material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+		material.SetInt("_ZWrite", 1);
+		material.DisableKeyword("_ALPHATEST_ON");
+		material.DisableKeyword("_ALPHABLEND_ON");
+		material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+		material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Geometry;
+	}
+
 	private void FixAllNormalMaps()
 	{
 		foreach (var texture in textures)
 		{
 			if (texture == null || !texture.name.ToLower().Contains("_normal")) continue;
+
 			string path = AssetDatabase.GetAssetPath(texture);
 			TextureImporter textureImporter = AssetImporter.GetAtPath(path) as TextureImporter;
-
 			if (textureImporter != null)
 			{
 				textureImporter.textureType = TextureImporterType.NormalMap;
@@ -232,13 +255,14 @@ public class Source2UnityMatFixer : EditorWindow
 		}
 		Debug.Log("Normal map fixing complete.");
 	}
+
 	private void RemoveAllTexturesFromMaterials()
 	{
 		failedMaterials.Clear();
-
 		foreach (var material in materials)
 		{
 			if (material == null) continue;
+
 			material.mainTexture = null;
 			if (material.HasProperty("_BumpMap"))
 			{
